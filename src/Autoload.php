c86e0dfa07134e84d320b4e914dc63dbba929208
Dan Harrin <git@danharrin.com>
2023-07-15 14:42:01 +0100

    Support both versions

--- src/Autoload.php
+++ src/Autoload.php
@@ -5,15 +5,28 @@ declare(strict_types=1);
 namespace Pest\Livewire;
 
 use Livewire\Features\SupportTesting\Testable;
+use Livewire\Testing\TestableLivewire;
 use Pest\Plugin;
 
 Plugin::uses(InteractsWithLivewire::class);
 
-/**
- * @return Testable
- */
-function livewire(string $name, array $params = [])
-{
-    // @phpstan-ignore-next-line
-    return test()->livewire(...func_get_args());
+
+if (class_exists(Testable::class)) {
+    /**
+     * @return Testable
+     */
+    function livewire(string $name, array $params = [])
+    {
+        // @phpstan-ignore-next-line
+        return test()->livewire(...func_get_args());
+    }
+} else {
+    /**
+     * @return TestableLivewire
+     */
+    function livewire(string $name, array $params = [])
+    {
+        // @phpstan-ignore-next-line
+        return test()->livewire(...func_get_args());
+    }
 }
--- src/InteractsWithLivewire.php
+++ src/InteractsWithLivewire.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace Pest\Livewire;
 
 use Livewire\Features\SupportTesting\Testable;
+use Livewire\Testing\TestableLivewire;
 use Livewire\Livewire;
 
-trait InteractsWithLivewire
-{
-    /** @param  array<string, mixed>  $params */
-    public function livewire(string $name, array $params = []): Testable
+if (class_exists(Testable::class)) {
+    trait InteractsWithLivewire
     {
-        return Livewire::test($name, $params);
+        /** @param  array<string, mixed>  $params */
+        public function livewire(string $name, array $params = []): Testable
+        {
+            return Livewire::test($name, $params);
+        }
+    }
+} else {
+    trait InteractsWithLivewire
+    {
+        /** @param  array<string, mixed>  $params */
+        public function livewire(string $name, array $params = []): TestableLivewire
+        {
+            return Livewire::test($name, $params);
+        }
     }
 }
