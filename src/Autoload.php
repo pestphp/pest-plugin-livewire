d7be308f611417c07c18a8ba331269a008ab1106
Robbin "Roboroads" Schepers <Roboroads@users.noreply.github.com>
2023-07-25 15:18:44 +0200

    fix: Fixed PHPStan error

--- src/Autoload.php
+++ src/Autoload.php
@@ -10,22 +10,8 @@ use Pest\Plugin;
 
 Plugin::uses(InteractsWithLivewire::class);
 
-if (class_exists(Testable::class)) {
-    /**
-     * @return Testable // @phpstan-ignore-next-line
-     */
-    function livewire(string $name, array $params = [])
-    {
-        // @phpstan-ignore-next-line
-        return test()->livewire(...func_get_args());
-    }
-} else {
-    /**
-     * @return TestableLivewire
-     */
-    function livewire(string $name, array $params = [])
-    {
-        // @phpstan-ignore-next-line
-        return test()->livewire(...func_get_args());
-    }
+function livewire(string $name, array $params = [])
+{
+    // @phpstan-ignore-next-line
+    return test()->livewire(...func_get_args());
 }
