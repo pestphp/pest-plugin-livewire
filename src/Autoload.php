e2f2edb0a7d414d6837d87908a0e148256d3bf89
Nuno Maduro <enunomaduro@gmail.com>
2024-09-09 01:05:59 +0100

    chore: adjusts dependencies

--- src/Autoload.php
+++ src/Autoload.php
@@ -5,27 +5,15 @@ declare(strict_types=1);
 namespace Pest\Livewire;
 
 use Livewire\Features\SupportTesting\Testable;
-use Livewire\Testing\TestableLivewire;
 use Pest\Plugin;
 
 Plugin::uses(InteractsWithLivewire::class);
 
-if (class_exists(Testable::class)) {
-    /**
-     * @return Testable
-     */
-    function livewire(string $name, array $params = [])
-    {
-        // @phpstan-ignore-next-line
-        return test()->livewire(...func_get_args());
-    }
-} else {
-    /**
-     * @return TestableLivewire // @phpstan-ignore-next-line
-     */
-    function livewire(string $name, array $params = [])
-    {
-        // @phpstan-ignore-next-line
-        return test()->livewire(...func_get_args());
-    }
+/**
+ * @return Testable
+ */
+function livewire(string $name, array $params = [])
+{
+    // @phpstan-ignore-next-line
+    return test()->livewire(...func_get_args());
 }
--- src/InteractsWithLivewire.php
+++ src/InteractsWithLivewire.php
@@ -6,24 +6,12 @@ namespace Pest\Livewire;
 
 use Livewire\Features\SupportTesting\Testable;
 use Livewire\Livewire;
-use Livewire\Testing\TestableLivewire;
 
-if (class_exists(Testable::class)) {
-    trait InteractsWithLivewire
+trait InteractsWithLivewire
+{
+    /** @param  array<string, mixed>  $params */
+    public function livewire(string $name, array $params = []): Testable
     {
-        /** @param  array<string, mixed>  $params */
-        public function livewire(string $name, array $params = []): Testable
-        {
-            return Livewire::test($name, $params);
-        }
-    }
-} else {
-    trait InteractsWithLivewire
-    {
-        /** @param  array<string, mixed>  $params */
-        public function livewire(string $name, array $params = []): TestableLivewire
-        {
-            return Livewire::test($name, $params);
-        }
+        return Livewire::test($name, $params);
     }
 }
